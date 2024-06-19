6357ac8304d087a1885f3851e693f0dbdcb68f3c
User <89028637729@mail.ru>
2024-06-19 16:37:50 +0500

    Объявл переменные типа int, byte, short, long, float, double.Вывел значения всех переменных в консоль.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-
-
+        System.out.println("Задача 1");
+        byte a = 126;
+        System.out.println ("Значение переменной a с типом byte равно " + a);
+        short b = 32766;
+        System.out.println ("Значение переменной b с типом short равно " + b);
+        int c = 2147483646;
+        System.out.println ("Значение переменной c с типом int равно " + c);
+        long d = 859152555;
+        System.out.println ("Значение переменной d с типом long равно " + d);
+        float e = 3.75f;
+        System.out.println ("Значение переменной e с типом float равно " + e);
+        double f =1.889165165194;
+        System.out.println ("Значение переменной f с типом double равно " + f);
     }
 }
